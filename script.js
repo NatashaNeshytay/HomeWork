98c9034df1c6920660b6851507ec2a0a61d3f6c6
NatashaNeshytay <natasha_gomel@rambler.ru>
2023-02-03 00:16:13 +0300

    created home#4

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-let studentIt = [
+const str = 'My name is Natallia';
+
+console.log(str.repeat(3));
+console.log(str.includes('n'));
+console.log(str.lastIndexOf('is'));
+console.log(str.slice(3,));
+console.log(str.slice(3, 7));
+
+
+const studentIt = [
     {firstName: 'Иван',
     lastName:'Иванов',
     age: 18},
@@ -8,7 +17,35 @@ let studentIt = [
     {firstName: 'Лена',
     lastName:'Сидорова',
     age: 44}
-]
+];
 
-let names = studentIt.map( studentIt => studentIt.firstName());
+const studenIt2 = [
+    {firstName: 'Света',
+    lastName:'Герасимова',
+    age: 31}
+];
+
+const names = studentIt.map(elem => elem.firstName);
 console.log(names);
+
+const age = studentIt.filter(elem => elem.age > 20);
+console.log(age);
+
+const age2 = studentIt.every(elem => elem.age >= 18);
+console.log(age2);
+
+const studentItNew = studentIt.concat(studenIt2);
+console.log(studentItNew);
+
+const student = studentIt.sort(function (a, b) {
+    if (a.firstName > b.firstName) {
+        return 1;
+    }
+    if (a.firstName < b.firstName) {
+        return -1;
+    }
+    return 0;
+});
+
+console.log(student);
+
